a1ef83a8d0a15c74c9de83a5477cb9eeeaf27aed
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-02-08 13:15:52 -0300

    Create Footer.jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <img src="img/Logo.png" alt="" />
+      </div>
+      <div>
+        <p>4687-8417//2126-7353 // info@famox.com.ar</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
