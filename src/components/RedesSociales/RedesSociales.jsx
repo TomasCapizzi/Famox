390a0f5495cf09ad1e4a3fe3d7f92296986985c0
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-02-08 13:16:24 -0300

    Create RedesSociales.jsx

---
+++ src/components/RedesSociales/RedesSociales.jsx
@@ -0,0 +1,13 @@
+import {FaInstagram, FaLinkedin, FaWhatsapp} from 'react-icons/fa'
+
+import React from 'react';
+
+function RedesSociales() {
+  return <div className='rrss'>
+      <a href="https://api.whatsapp.com/send?phone=5491134276306&text=Hola%20Famox!%20Estoy%20interesado%20en%20consultar%20sobre..." target='_blank' rel="noreferrer"><FaWhatsapp id='wpp'/></a>
+      <a href="https://www.instagram.com/famox_sa/?hl=es-la" target='_blank' rel="noreferrer"><FaInstagram id='insta'/></a>
+      <a href="https://www.linkedin.com/company/famoxsa/" target='_blank' rel="noreferrer"><FaLinkedin id='lin'/></a>
+  </div>;
+}
+
+export default RedesSociales;
