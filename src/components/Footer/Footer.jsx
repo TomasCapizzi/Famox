3d0ed6beebe8256ddf9259d1050ed51d5771b935
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-02-15 11:37:46 -0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,33 @@
+import {Link} from 'react-router-dom'
 import React from 'react';
 
 function Footer() {
   return (
     <footer>
-      <div>
+      <div className='footer-cabecera'>
         <img src="img/Logo.png" alt="" />
       </div>
-      <div>
-        <p>4687-8417//2126-7353 // info@famox.com.ar</p>
+      <div className='footer-info'>
+        <div className='footer-links'>
+          <h5>Famox SA</h5>
+          <Link to='/nosotros'>
+            <p>¿Quiénes somos?</p>
+          </Link>
+
+          <Link to='/preguntas-frecuentes'>
+            <p>Preguntas frecuentes</p>
+          </Link>
+
+          <Link to='/productos'>
+            <p>Nuestros productos</p>
+          </Link>
+        </div>
+        <div className='footer-contacto'>
+          <h5>Contáctanos</h5>
+          <p>4687-8417//2126-7353</p>
+          <p>info@famox.com.ar</p>
+          <p>atencion.cliente@famox.com.ar</p>
+        </div>
       </div>
     </footer>
   )
