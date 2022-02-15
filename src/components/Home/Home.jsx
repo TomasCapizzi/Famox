14399895fb922e157a0a49a6710b74f5fe22dbe4
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-02-15 11:38:23 -0300

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Carrusel from './Carrusel';
 import Categoria from './Categoria/Categoria';
+import Categorias from './Categorias';
 import React from 'react';
 import RedesSociales from '../RedesSociales/RedesSociales';
 
@@ -7,11 +8,7 @@ function Home() {
   return (
   <div className='home'>
     <Carrusel/>
-    <div className='categorias-container'>
-      <Categoria titulo='Unidades de Suministro' img='img/Categorias/unidadesSuministro.jpg' link='/unidades-de-suministro' />
-      <Categoria titulo='Gasoterapia' img='img/Categorias/gasoterapia.jpg' link='/gasoterapia' />
-      <Categoria titulo='Accesorios' img='img/Categorias/accesorios.jpg' link='accesorios' />
-    </div>
+    <Categorias />
     <div className='rrss-container'><RedesSociales/></div>
   </div>
   )
