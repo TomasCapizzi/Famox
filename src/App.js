d941bb1ce0068b50998a6b7a6f542437e92cd709
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-02-15 11:34:56 -0300

    Update App.js
    Menu routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './styles/main.scss';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 import Accesorios from './components/ListaProductos/Accesorios/Accesorios';
+import Certificaciones from './components/Certificaciones/Certificaciones';
+import Contacto from './components/Contacto/Contacto';
 import CrearProducto from './components/CrearProducto/CrearProducto';
 import DetalleGasoterapia from './components/Detalle/Gasoterapia/DetalleGasoterapia';
 import DetalleUnidSum from './components/Detalle/Unidades de Suministro/DetalleUnidSum';
+import Ecommerce from './components/Ecommerce/Ecommerce';
 import Footer from './components/Footer/Footer';
 import Gasoterapia from './components/ListaProductos/Gasoterapia/Gasoterapia';
+import Historia from './components/Historia/Historia';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 import Menu from './components/Menu/Menu';
+import Productos from './components/ListaProductos/Productos';
 import React from 'react'
 import UnidadesSuministro from './components/ListaProductos/UnidadesSuministro/UnidadesSuministro';
 
@@ -20,6 +26,14 @@ function App() {
         <Menu/>
         <Routes>
           <Route path='/' element={<Home/>} />
+
+          <Route path='/nosotros' element={<Historia/>} />
+          <Route path='/certificaciones' element={<Certificaciones/>} />
+          <Route path='/contacto' element={<Contacto/>} />
+          <Route path='/productos' element={<Productos/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/ecommerce' element={<Ecommerce/>} />
+
           <Route path='/gasoterapia' element={<Gasoterapia/>}/>
           <Route path='/unidades-de-suministro' element={<UnidadesSuministro/>}/>
           <Route path='/accesorios' element={<Accesorios/>}/>
@@ -28,6 +42,7 @@ function App() {
           <Route path='/gasoterapia/:id' element={<DetalleGasoterapia/>} />
 
           <Route path='/crear-producto' element={<CrearProducto/>} />
+
         </Routes>
         <Footer/>
       </div>
