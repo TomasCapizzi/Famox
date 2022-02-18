9a4fdfafe572d6a67c7ba1752771f5b3445fceb9
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-02-18 14:26:39 -0300

    Data

---
+++ src/data/conectores.js
@@ -0,0 +1,36 @@
+const CONECTORES = [
+    {
+        img: 'img/Conectores/PAG-DISS.png',
+        nombre: 'DISS',
+    },
+    {
+        img: 'img/Conectores/PAG-AFNOR.png',
+        nombre: 'AFNOR',
+    },
+    {
+        img: 'img/Conectores/PAG-SSAGA.png',
+        nombre: 'SS/AGA',
+    },
+    {
+        img: 'img/Conectores/PAG-OHMEDA.png',
+        nombre: 'OHMEDA',
+    },
+    {
+        img: 'img/Conectores/PAG-ON.png',
+        nombre: 'ON',
+    },
+    {
+        img: 'img/Conectores/PAG-CentralUni.png',
+        nombre: 'CU',
+    },
+    {
+        img: 'img/Conectores/IRAM.png',
+        nombre: 'IRAM',
+    },
+    {
+        img: 'img/Conectores/YUGO.png',
+        nombre: 'YUGO',
+    },
+];
+
+export default CONECTORES;
---
+++ src/data/gases.js
@@ -0,0 +1,33 @@
+const GASES = [
+    {
+        img: 'img/Gases/Aire.png',
+        nombre: 'AIRE',
+    },
+    {
+        img: 'img/Gases/CO2.png',
+        nombre: 'CO2',
+    },
+    {
+        img: 'img/Gases/N2.png',
+        nombre: 'N2',
+    },
+    {
+        img: 'img/Gases/N2O.png',
+        nombre: 'N2O',
+    },
+    {
+        img: 'img/Gases/Oxigeno.png',
+        nombre: 'OXIGENO',
+    },
+    {
+        img: 'img/Gases/Aire_Variable.png',
+        nombre: 'OXIGENO/AIRE',
+    },
+    {
+        img: 'img/Gases/Vacio.png',
+        nombre: 'VACIO',
+    },
+
+];
+
+export default GASES;
