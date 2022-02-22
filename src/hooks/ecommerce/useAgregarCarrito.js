a21c9dc99e6c3ada72a6353a63e84f41d184fb87
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-02-22 20:21:33 -0300

    Update useAgregarCarrito.js
    Validate items and id's

--- src/hooks/ecommerce/useAgregarCarrito.js
+++ src/hooks/ecommerce/useAgregarCarrito.js
@@ -4,23 +4,39 @@ import { CarritoContext } from '../../store/carritoContext';
 
 const useAgregarCarrito = ({item}) => {
 
-    const {agregarItem} = useContext(CarritoContext);
+    const {agregarItem, carro} = useContext(CarritoContext);
     
     const [gas, setGas] = useState();
     const [conector, setConector] = useState();
 
     function agregarAlCarrito(cantidad){
-        console.log(cantidad);
         const {nombre, img, _id} = item
-        const producto = {
-          nombre,
-          gas,
-          conector,
-          img,
-          cantidad,
-          _id
+        const coincidencia = carro.find(
+          producto => (producto._id === _id) && ((item.gas !== producto.gas) || (item.conector !== producto.conector))
+        )
+        if(coincidencia){
+          console.log('MISMO ID PERO DIFERENTE PROD')
+          const nuevoID = (Math.random()*100 + 1).toString();
+          const producto = {
+            nombre,
+            gas,
+            conector,
+            img,
+            cantidad,
+            _id: nuevoID
+          }
+          agregarItem({producto})
+        } else {
+          const producto = {
+            nombre,
+            gas,
+            conector,
+            img,
+            cantidad,
+            _id
+          }
+          agregarItem({producto})
         }
-        agregarItem({producto})
       }
 
 
