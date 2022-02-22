b718ba0066de2654ad6cfb3652a986f5b31ce1df
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-02-22 20:13:59 -0300

    Update Carrito.jsx
    Table display

--- src/components/Carrito/Carrito.jsx
+++ src/components/Carrito/Carrito.jsx
@@ -1,23 +1,55 @@
-import React, {useContext} from 'react';
+import React, {useContext, useRef, useState} from 'react';
 
 import {CarritoContext} from '../../store/carritoContext';
 import CarritoIem from './CarritoItem';
+import FormularioCotizacion from './FormularioCotizacion';
 
 function Carrito() {
 
     const {carro} = useContext(CarritoContext);
+    const [formHandler, setFormHandler] = useState(false)
+    const formRef = useRef();
+
+    function habilitarForm(){
+        setFormHandler(!formHandler);
+        formRef.current.classList.toggle('off')
+    }
+
+    function pedirCotizacion(e){
+        e.preventDefault()
+    }
   return (
     <section className='carrito'>
         <h1>Carrito</h1>
-        <div className='carrito-container}'>
-        { 
-            carro.length >= 1 ?
-                carro.map(
-                    item => <CarritoIem item={item} key={item._id} />
-                )
-
-            : <p>Tu carrito esta vacio</p>
-        }
+        <div className='carrito-container'>
+            {
+                carro.length === 0 ?
+                <p>Tu carrito esta vacio ...</p>
+                : 
+                <>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Artículo</th>
+                                <th>Cantidad</th>
+                                <th>Borrar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { 
+                                carro.length >= 1 &&
+                                    carro.map(
+                                        item => <CarritoIem item={item} key={item._id} />
+                                    )
+                            }                    
+                        </tbody>   
+                    </table>
+                    {formHandler ? null : <button onClick={habilitarForm}>Pedir Cotización</button> }
+                    
+                </>
+                
+            }
+            <FormularioCotizacion pedirCotizacion={pedirCotizacion} habilitarForm={habilitarForm}  formRef={formRef} />
         </div>  
     </section>
   )
