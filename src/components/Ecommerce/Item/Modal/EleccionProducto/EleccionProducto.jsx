93ff86b7a1066c0d33ffb15d80e6f21cf4249205
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-02-22 20:19:37 -0300

    Update EleccionProducto.jsx
    Validate options

--- src/components/Ecommerce/Item/Modal/EleccionProducto/EleccionProducto.jsx
+++ src/components/Ecommerce/Item/Modal/EleccionProducto/EleccionProducto.jsx
@@ -7,7 +7,7 @@ import React from 'react'
 import {TiDelete} from 'react-icons/ti';
 import useAgregarCarrito from '../../../../../hooks/ecommerce/useAgregarCarrito'
 
-function EleccionProducto({item}) {
+function EleccionProducto({item,toggleModal }) {
 
     const {agregarAlCarrito, setConector, setGas, conector, gas} = useAgregarCarrito({item})
 
@@ -22,44 +22,70 @@ function EleccionProducto({item}) {
     <div className='modal-eleccion'>
         <div className='container' >
           {
-            item.conector ? 
+            item.conexion ? 
               <Conectores item={item} setConector={setConector} />
             : null
           }
           {
-            item.gases ?             
+            item.gas ?             
               <Gases item={item} setGas={setGas} />
             : null
           }
 
-          <div className='selecciones'>
-            <p>Tu selección</p>
-            <div className='container'>
-              {
-                conector && CONECTORES.map(
-                  item => item.nombre === conector ? 
-                    <div key={item.nombre} className='seleccionado'>
-                      <button><TiDelete onClick={eliminarSeleccionConector} /></button>
-                      <img src={item.img} alt="" />
-                      <p>{item.nombre}</p>
-                    </div>
-                  : null
-                )
-              }
-              {
-                gas && GASES.map(
-                  item => item.nombre === gas ? 
-                    <div key={item.nombre} className='seleccionado' >
-                      <button><TiDelete onClick={eliminarSeleccionGas} /></button>
-                      <img src={item.img} alt="" />
-                      <p>{item.nombre}</p>
-                    </div>
-                    : null
-                )
-              }
-            </div>
-          </div>
-          <Botonera agregarAlCarrito={agregarAlCarrito} />
+          {
+            item.gas || item.conexion ? 
+              <div className='selecciones'>
+                {
+                  gas || conector ? <p>Tu selección</p> : null
+                }
+                <div className='container'>
+                  {
+                    conector && CONECTORES.map(
+                      item => item.nombre === conector ? 
+                        <div key={item.nombre} className='seleccionado'>
+                          <button><TiDelete onClick={eliminarSeleccionConector} /></button>
+                          <img src={item.img} alt="" />
+                          <p>{item.nombre}</p>
+                        </div>
+                      : null
+                    )
+                  }
+                  {
+                    gas && GASES.map(
+                      item => item.nombre === gas ? 
+                        <div key={item.nombre} className='seleccionado' >
+                          <button><TiDelete onClick={eliminarSeleccionGas} /></button>
+                          <img src={item.img} alt="" />
+                          <p>{item.nombre}</p>
+                        </div>
+                        : null
+                    )
+                  }
+                </div>
+              </div> 
+            : null
+            }
+
+            {
+              (item.gas && item.conexion) ? 
+                (conector && gas) ?
+                  <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
+                 : <p>Primero debes seleccionar el tipo de gas y su conexión</p>
+
+              : (item.gas && item.conexion === false) ? 
+                (gas) ?
+                  <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
+                 : <p>Selecciona el tipo de gas</p>
+
+              : (item.gas === false && item.conexion) ?
+                (conector) ?
+                  <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
+                : <p>Selecciona el tipo de conector</p>
+
+              :  <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
+            }
+
+            
         </div>
     </div>
   )
