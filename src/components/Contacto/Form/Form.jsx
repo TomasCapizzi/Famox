09a5ffcdf24f76cf497fc35158044c739752b942
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-03-07 11:44:39 -0300

    Update Form.jsx

--- src/components/Contacto/Form/Form.jsx
+++ src/components/Contacto/Form/Form.jsx
@@ -16,19 +16,22 @@ function Form() {
     }
 
   return (
-    <form action="" onSubmit={(e)=> enviarConsulta(e)} >
+    <form action="http://localhost:4000/formulario/contacto" method='POST' onSubmit={(e)=> enviarConsulta(e)} >
 
     <label htmlFor="">Nombre</label>
-    <input type="text" id='nombre' />
+    <input type="text" id='nombre' name='nombre' />
+
+    <label htmlFor="">Empresa</label>
+    <input type="text" id='empresa' name='empresa' />
 
     <label htmlFor="">Correo Electrónico</label>
-    <input type="email"  id='mail' />
+    <input type="email"  id='mail' name='email' />
 
     <label htmlFor="">Asunto</label>
-    <input type="text"  id='asunto' />
+    <input type="text"  id='asunto' name='asunto' />
 
     <label htmlFor="">Mensaje</label>
-    <textarea name="" id="mensaje"></textarea>
+    <textarea name="mensaje" id="mensaje"></textarea>
 
     <input type="submit" value='Enviar' className='submit' />
 
