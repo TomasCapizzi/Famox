4485e2e886753d5b74c9f69a874d5e4c762d0f6c
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-03-07 11:44:54 -0300

    Update Gases.jsx

--- src/components/Detalle/Gasoterapia/Descripcion/Gases.jsx
+++ src/components/Detalle/Gasoterapia/Descripcion/Gases.jsx
@@ -1,33 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import GASES from '../../../../data/gases';
 
 function Gases({product}) {
 
-    const {gases} = product
+    const [img, setImg] = useState()
+
     return (
         <article className='gases'>
             <h4>Gases Disponibles</h4>
             <div>
                 {
-                    gases.aire && <img src={'/img/Gases/Aire.png'} alt="aire" />
+                    product.map(
+                        item => <img src={'/'+ GASES.filter(gas => gas.nombre === item)[0].img} alt={item} key={item} />
+                    )
                 }
-                {
-                    gases.co2 && <img src={'/img/Gases/CO2.png'} alt='co2' /> 
-                }
-                {
-                    gases.n2 && <img src={'/img/Gases/N2.png'} alt='n2' /> 
-                }
-                {
-                    gases.n2o && <img src={'/img/Gases/N2O.png'} alt='n2o' /> 
-                }
-                {
-                    gases.oxigeno && <img src={'/img/Gases/Oxigeno.png'} alt='oxigeno' /> 
-                }
-                {
-                    gases.oxigeno_aire && <img src={'/img/Gases/Aire_Variable.png'} alt='oxigeno/aire' /> 
-                }
-                {
-                    gases.vacio && <img src={'/img/Gases/Vacio.png'} alt='vacio' /> 
-                }
+                
             </div>
         </article>    
     )
