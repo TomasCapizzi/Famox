c067b048dd05805dff3548f8fef4ebac422a1ae6
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-03-07 11:44:50 -0300

    Update Conectores.jsx

--- src/components/Detalle/Gasoterapia/Descripcion/Conectores.jsx
+++ src/components/Detalle/Gasoterapia/Descripcion/Conectores.jsx
@@ -1,3 +1,4 @@
+import CONECTORES from '../../../../data/conectores';
 import React from 'react';
 
 function Conectores({product}) {
@@ -8,60 +9,13 @@ function Conectores({product}) {
         <h4>Conectores</h4>
         <div className='conec-info'>
             {
-                conector.diss && 
-                <div>
-                    <h5>DISS</h5>
-                    <img src={'/img/Conectores/PAG-DISS.png'} alt="diss" />
-                </div>
-            }
-            {
-                conector.afnor && 
-                    <div>
-                        <h5>AFNOR</h5>
-                        <img src={'/img/Conectores/PAG-AFNOR.png'} alt="afnor" />
+                product.map(
+                    item => 
+                    <div key={item}>
+                        <h5>{item}</h5>
+                        <img src={'/' + CONECTORES.filter(conec=> conec.nombre === item)[0].img} alt={item.nombre} />
                     </div>
-            }
-            {
-                conector.ss_aga &&
-                    <div>
-                        <h5>SS/AGA</h5>
-                        <img src={'/img/Conectores/PAG-SSAGA.png'} alt="ss_aga" />
-                    </div>
-            }
-            {
-                conector.ohmeda && 
-                    <div>
-                        <h5>OHMEDA</h5>
-                        <img src={'/img/Conectores/PAG-OHMEDA.png'} alt="ohmeda" />
-                    </div>
-            }
-            {
-                conector.on && 
-                    <div>
-                        <h5>ON</h5>
-                        <img src={'/img/Conectores/PAG-ON.png'} alt="on" />
-                    </div>
-            }
-            {
-                conector.cu && 
-                    <div>
-                        <h5>C.U.</h5>
-                        <img src={'/img/Conectores/PAG-CentralUni.png'} alt="cu" />
-                    </div>
-            }
-            {
-                conector.iram &&
-                    <div>
-                        <h5>IRAM</h5>
-                        <img src={'/img/Conectores/IRAM.png'} alt="iram" />
-                    </div>
-            }
-            {
-                conector.yugo && 
-                    <div>
-                        <h5>YUGO</h5>
-                        <img src={'/img/Conectores/YUGO.png'} alt="yugo" />
-                    </div>
+                )
             }
         </div>
     </article>
