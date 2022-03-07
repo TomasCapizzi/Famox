d9e3e50ec53ec162380043dc275cef5d59ce6dcf
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-03-07 11:44:04 -0300

    Update CarritoItem.jsx
    Add Cart item descriptions

--- src/components/Carrito/CarritoItem.jsx
+++ src/components/Carrito/CarritoItem.jsx
@@ -2,18 +2,48 @@ import React, {useContext} from 'react';
 
 import {BsTrashFill} from 'react-icons/bs';
 import {CarritoContext} from '../../store/carritoContext';
+import useCodigo from '../../hooks/codigos/useCodigos';
 
 function CarritoItem({item}) {
 
+  const {derivarFuncion} = useCodigo(item)
+  console.log(item);
+
+  const codigo =  derivarFuncion();
   const {removerItem} = useContext(CarritoContext);
 
   return (
     <tr className='carrito-item'>
-        <td><img src={item.img} alt="" /></td>
-        <td><p>{item.nombre}</p></td>
-        <td><p>{item.cantidad}</p></td>
-        <td><BsTrashFill onClick={()=> removerItem(item)} /></td>
-        
+      <td><img src={item.img} alt="" /></td>
+      <td><p>{item.nombre}</p></td>
+      <td><p>{codigo}</p></td>
+      <td><p>{item.cantidad}</p></td>
+      <td><BsTrashFill onClick={()=> removerItem(item)} /></td>
+      {
+        item.gas &&       
+          <td className='filter'>
+            <p>Gas: {item.gas}</p>
+          </td>
+      }
+      {
+        item.conector && 
+          <td className='filter'>
+            <p>Conector: {item.conector}</p>
+          </td>
+      }
+      {
+        item.rango &&
+          <td className='filter'>
+            <p>Rango: {item.rango}</p>
+          </td>
+      }
+      {
+        item.modelo &&
+          <td className='filter'>
+            <p>{(item.nombre === 'Recipiente Colector' || item.nombre === 'Mezclador de Aire/Oxígeno') ?  item.modelo.rango : item.modelo.nombre}</p>
+          </td>
+      }
+      
     </tr>
   )
 }
