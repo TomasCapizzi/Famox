59bac17115223363aaab1106690fb42018635698
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-03-07 11:45:08 -0300

    Update Modelos.jsx

--- src/components/Detalle/Modelos/Modelos.jsx
+++ src/components/Detalle/Modelos/Modelos.jsx
@@ -1,44 +1,17 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 
 import ModeloItem from './ModeloItem';
 import Spinner from '../../Spinner/Spinner';
 
-function Modelos({modelo}) {
-
-    const [modelos, setModelos] = useState([]);
-    console.log(modelo);
-    const api = 'https://famox-api.herokuapp.com/api/products/modelos'
-    async function getModelos(){
-        const response = await fetch(api);
-        const res = await response.json();
-        console.log(res.products);
-        setModelos(res.products);
-    }
-
-    useEffect(()=>{
-        getModelos()
-    },[])
-
+function Modelos({modelos}) {
+    console.log(modelos);
   return (
     <article className='modelos'>
         {
             modelos.length > 0 ? 
-                modelo === 'Recipiente Colector' ?
-                    <div className='container'>
-                        {
-                            modelos.map(
-                                item => item.origen === 'recipiente_colector' && <ModeloItem item={item} key={item._id} />
-                            )
-                        }
-                    </div>
-                :
-                    <div className='container'>
-                        {
-                            modelos.map(
-                                item => item.origen === 'mezclador' && <ModeloItem item={item} key={item._id} />
-                            )
-                        }
-                    </div>
+                modelos.map(
+                    item => <ModeloItem item={item} key={item._id} />
+                )
             : <Spinner />
         }
     </article>
