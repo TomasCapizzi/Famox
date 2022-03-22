dcd5187af88a02d3031b189c4a7aaefbeb025509
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-03-22 11:31:35 -0300

    Update FormularioCotizacion.jsx

--- src/components/Carrito/FormularioCotizacion.jsx
+++ src/components/Carrito/FormularioCotizacion.jsx
@@ -1,18 +1,20 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 function FormularioCotizacion({pedirCotizacion, habilitarForm, formRef}) {
 
+//method='POST' target='_blank'
   return (
-    <form onSubmit={(e)=> pedirCotizacion(e)} className='form-cotizacion off' ref={formRef} >
+    <form action='' className='form-cotizacion off' ref={formRef}   onSubmit={(e)=> pedirCotizacion(e)} >
         <label htmlFor="nombre">Nombre</label>
         <input type="text" name='nombre' id='nombre'  />
+        <label htmlFor="nombre">Empresa</label>
+        <input type="text" name='empresa' id='empresa'  />
         <label htmlFor="mail">Mail</label>
         <input type="text" id='mail' name='mail' placeholder='correo@gmail.com' />
-
-        <input type="submit" value='Pedir cotización' />
+        <input type="submit" value='Pedir cotización' className='btn-submit' />
         <button onClick={()=> habilitarForm()}>Seguir comprando</button>
     </form>
   )
 }
 
-export default FormularioCotizacion
+export default FormularioCotizacion;
