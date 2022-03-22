884df463d2db5ff705b69d7aa18b58f379a1aeb2
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-03-22 11:34:02 -0300

    Update EleccionGasoterapia.jsx
    Fix buttons display and validation

--- src/components/Ecommerce/Item/Modal/EleccionProducto/Gasoterapia/EleccionGasoterapia.jsx
+++ src/components/Ecommerce/Item/Modal/EleccionProducto/Gasoterapia/EleccionGasoterapia.jsx
@@ -99,41 +99,40 @@ function EleccionGasoterapia({item,toggleModal }) {
                     <p>{modelo.nombre}</p>
                   </div>
                : null
-            }
-        
-            { // Habilitacion de Botonera
-
-              (item.gases_ && item.conectores && !item.modelos && item.rangos) ?
-                (conector && gas && rango) ?
-                  <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
-                 : <p>Primero debes seleccionar el tipo de gas y su conexión y rango</p>
-
-            : (!item.rangos && item.gas && item.conexion && !item.modelos ) ?
-                (conector && gas)? <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
-                : <p>Primero debes seleccionar el tipo de gas y su conexión</p>
-
-            : (item.gas && item.conexion === false) ? 
-              (gas) ?
-                <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
-              : <p>Selecciona el tipo de gas</p>
-
-            : (item.gas === false && item.conexion) ?
-              (conector) ?
-                <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
-              : <p>Selecciona el tipo de conector</p>
-
-            : (item.modelos && !item.gas && !item.conexion ) ?
-              modelo ? <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
-              : <p>Elige el modelo</p>
-
-            : (item.gas && item.conexion && item.modelos) ?
-              (conector && gas && modelo) ? 
-              <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
-              : <p>Primero debes seleccionar el tipo de gas,su conexión y el modelo</p>
-
-            : <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
             }            
         </div>
+        { // Habilitacion de Botonera
+
+          (item.gases_ && item.conectores && !item.modelos && item.rangos) ?
+            (conector && gas && rango) ?
+              <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
+            : <p>Primero debes seleccionar el tipo de gas y su conexión y rango</p>
+
+          : (!item.rangos && item.gas && item.conexion && !item.modelos ) ?
+            (conector && gas)? <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
+            : <p>Primero debes seleccionar el tipo de gas y su conexión</p>
+
+          : (item.gas && item.conexion === false) ? 
+          (gas) ?
+            <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
+          : <p>Selecciona el tipo de gas</p>
+
+          : (item.gas === false && item.conexion) ?
+          (conector) ?
+            <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
+          : <p>Selecciona el tipo de conector</p>
+
+          : (item.modelos && !item.gas && !item.conexion ) ?
+          modelo ? <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
+          : <p>Elige el modelo</p>
+
+          : (item.gas && item.conexion && item.modelos) ?
+          (conector && gas && modelo) ? 
+          <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
+          : <p>Primero debes seleccionar el tipo de gas,su conexión y el modelo</p>
+
+          : <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
+          }
     </div>
   )
 }
