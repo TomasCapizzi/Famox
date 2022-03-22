c9832679beb0b55906d3ac7fda5e464f80408f29
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-03-22 11:41:14 -0300

    Update IluminacionItem.jsx

--- src/components/Ecommerce/Item/Modal/EleccionProducto/UnidSum/Items/IluminacionItem.jsx
+++ src/components/Ecommerce/Item/Modal/EleccionProducto/UnidSum/Items/IluminacionItem.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function IluminacionItem({item}) {
+function IluminacionItem({item, obtenerCantidadInput}) {
   return (
     <article className='luz'>
     <p>{item}</p>
     <div>
         <p>Cantidad: </p>
-        <input type="number" name="cantidad" id="" />
+        <input type="number" name="cantidad" id="" placeholder='0' min={0} onChange={(e)=>obtenerCantidadInput(e, item)} />
     </div>
 </article>
   )
