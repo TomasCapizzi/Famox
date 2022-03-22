16ab2924cc1ddbb89d1832edf4721571899bb74f
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-03-22 11:31:57 -0300

    Update Contacto.jsx
    Toast notification

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -1,15 +1,27 @@
+import React,{useRef} from 'react'
+
 import Form from './Form/Form'
 import Info from './Info/Info'
-import React from 'react'
+import Notificacion from './Notificacion'
 import RedesSociales from '../RedesSociales/RedesSociales'
 
 function Contacto() {
+
+  const refNoti = useRef()
+
+  function mostrarNotificacion(){
+    refNoti.current.classList.toggle('on')
+    setTimeout(() => {
+      refNoti.current.classList.toggle('on')
+  }, 4000);
+  }
   return (
     <section className='contacto'>
         <h1>¡Contactanos!</h1>
         <div className='data'>
-            <Form/>
+            <Form mostrarNotificacion={mostrarNotificacion}/>
             <Info/>
+            <Notificacion refNoti={refNoti}/>
         </div>
         <RedesSociales/>
     </section>
