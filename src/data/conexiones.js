01c236ddd147259c3a8ef972dcd06e1ee3fb554f
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-04-04 09:36:54 -0300

    Conexiones -data

---
+++ src/data/conexiones.js
@@ -0,0 +1,19 @@
+export const GASES = [
+    'AIRE',
+    'CO2',
+    'N2',
+    'N2O',
+    'OXÍGENO',
+    'OXIGENO/AIRE',
+    'VACIO',
+];
+export const CONECTORES = [
+    'DISS',
+    'AFNOR',
+    'SS/AGA',
+    'OHMEDA',
+    'ON',
+    'CU',
+    'IRAM',
+    'YUGO'
+];
