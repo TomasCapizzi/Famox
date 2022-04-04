1eeca5b0787f8a8971b5466008a456244beaf57d
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-04-04 09:35:24 -0300

    Menu links components

---
+++ src/components/Menu/Links.jsx
@@ -0,0 +1,52 @@
+import {RiShoppingCart2Line, RiShoppingCartFill} from 'react-icons/ri';
+
+import {FaUser} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
+import {MdKeyboardArrowDown} from 'react-icons/md';
+import React from 'react';
+
+function Links({carro}) {
+
+  return (
+    <ul className='links'>
+        <Link to='/'>
+        <li>Famox</li>
+        </Link>
+        <Link  to='/ecommerce'>
+        <li>Ecommerce</li>
+        </Link>
+        <Link to='/nosotros'>
+        <li>Nosotros</li>            
+        </Link>
+        <div className='categorias-handler'>
+            <Link  to='/productos'><li>Productos <MdKeyboardArrowDown/></li></Link>
+            <div className='categorias'>
+            <Link to='/gasoterapia'><li>Gasoterapia</li></Link>
+            <Link to='/unidades-de-suministro'><li>Unidades de Suministro</li></Link>
+            <Link to='/accesorios'><li>Accesorios</li></Link>
+            </div>
+        </div>
+        <Link  to='/certificaciones'>
+        <li>Certificaciones</li>            
+        </Link>
+        <Link  to='/contacto'>
+        <li>Contacto</li>
+        </Link>
+        <Link to='/carrito' >
+        <li className='carrito'>{
+            carro.length > 0 ? <RiShoppingCartFill/> : <RiShoppingCart2Line/>
+        }
+        {
+            carro.length === 0 ?
+            null : <p>{carro.length}</p>
+        }
+        </li>
+        </Link>
+        <Link  to='/login'>
+        <li><FaUser/></li>
+        </Link>            
+    </ul>
+  )
+}
+
+export default Links
---
+++ src/components/Menu/LinksResponsive.jsx
@@ -0,0 +1,40 @@
+import {FaUser} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
+import {MdKeyboardArrowDown} from 'react-icons/md';
+import React from 'react';
+
+function LinksResponsive({menuUl, useMenuHamb}) {
+
+  return (
+    <ul className='responsive' ref={menuUl}>
+        <Link to='/'>
+        <li onClick={useMenuHamb}>Famox</li>
+        </Link>
+        <Link  to='/ecommerce'>
+        <li onClick={useMenuHamb}>Ecommerce</li>
+        </Link>
+        <Link to='/nosotros'>
+        <li onClick={useMenuHamb}>Nosotros</li>            
+        </Link>
+        <div className='categorias-handler'>
+            <Link  to='/productos'><li onClick={useMenuHamb}>Productos</li></Link>
+            <div className='categorias'>
+            <Link to='/gasoterapia'><li onClick={useMenuHamb}>Gasoterapia</li></Link>
+            <Link to='/unidades-de-suministro'><li onClick={useMenuHamb}>Unidades de Suministro</li></Link>
+            <Link to='/accesorios'><li onClick={useMenuHamb}>Accesorios</li></Link>
+            </div>
+        </div>
+        <Link  to='/certificaciones'>
+        <li onClick={useMenuHamb}>Certificaciones</li>            
+        </Link>
+        <Link  to='/contacto'>
+        <li onClick={useMenuHamb}>Contacto</li>
+        </Link>
+        <Link  to='/login'>
+        <li onClick={useMenuHamb}><FaUser/></li>
+        </Link>            
+    </ul>
+  )
+}
+
+export default LinksResponsive
