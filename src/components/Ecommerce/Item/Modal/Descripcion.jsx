c6d68654cb44c0bcb4a7c4f7596c81ef5f211bee
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-04-22 16:23:10 -0300

    Update Descripcion.jsx
    Img conditional rendering

--- src/components/Ecommerce/Item/Modal/Descripcion.jsx
+++ src/components/Ecommerce/Item/Modal/Descripcion.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 function Descripcion({item}) {
+  
+  const anchoPantalla = window.innerWidth;
+
   return (
     <div className='modal-descripcion'>
         <h3>{item.nombre}</h3>
-        <img src={item.img} alt="imagen" />
+        <img src={anchoPantalla>1024 && item.portada? item.portada : item.img} alt="imagen" />
     </div>
   )
 }
 
-export default Descripcion
+export default Descripcion;
