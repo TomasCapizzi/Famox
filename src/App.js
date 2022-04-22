8471fd33799eba48c47a4fb38e9d2df5e592f0c8
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-04-22 16:16:52 -0300

    Absolute Imports
    Change imports to absolute

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import './styles/main.scss';
 
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, HashRouter, Route, Routes} from 'react-router-dom'
 
-import Accesorios from './views/Productos/Accesorios/Accesorios';
-import Carrito from './components/Carrito/Carrito';
-import { CarritoContextProvider } from './store/carritoContext';
-import Certificaciones from './views/Certificaciones/Certificaciones';
-import Contacto from './views/Contacto/Contacto';
-import CrearProducto from './views/CrearProducto/CrearProducto';
-import DetalleGasoterapia from './views/ProductoDetalle/DetalleGasoterapia';
-import DetalleUnidSum from './views/ProductoDetalle/DetalleUnidSum';
-import Ecommerce from './views/Ecommerce/Ecommerce';
-import Footer from './components/Footer/Footer';
-import Gasoterapia from './views/Productos/Gasoterapia/Gasoterapia';
-import Historia from './views/Nosotros/Historia';
-import Home from './views/Home/Home';
-import Login from './views/Login/Login';
-import Menu from './components/Menu/Menu';
-import PregFrec from './views/PregFrec/PregFrec';
-import Productos from './views/Productos/Productos';
+import Accesorios from 'views/Productos/Accesorios/Accesorios';
+import Carrito from 'views/Carrito/Carrito';
+import { CarritoContextProvider } from 'store/carritoContext';
+import Certificaciones from 'views/Certificaciones/Certificaciones';
+import CheckOut from 'views/CheckOut/CheckOut';
+import Contacto from 'views/Contacto/Contacto';
+import CrearProducto from 'views/CrearProducto/CrearProducto';
+import DetalleGasoterapia from 'views/ProductoDetalle/DetalleGasoterapia';
+import DetalleUnidSum from 'views/ProductoDetalle/DetalleUnidSum';
+import Ecommerce from 'views/Ecommerce/Ecommerce';
+import Footer from 'components/Footer/Footer';
+import Gasoterapia from 'views/Productos/Gasoterapia/Gasoterapia';
+import Historia from 'views/Nosotros/Historia';
+import Home from 'views/Home/Home';
+import Login from 'views/Login/Login';
+import Menu from 'components/Menu/Menu';
+import PregFrec from 'views/PregFrec/PregFrec';
+import Productos from 'views/Productos/Productos';
 import React from 'react'
-import UnidadesSuministro from './views/Productos/UnidadesSuministro/UnidadesSuministro';
+import UnidadesSuministro from 'views/Productos/UnidadesSuministro/UnidadesSuministro';
 
 function App() {
   return (
@@ -47,6 +48,7 @@ function App() {
             <Route path='/unidades-de-suministro/:id' element={<DetalleUnidSum/>} />
             <Route path='/gasoterapia/:id' element={<DetalleGasoterapia/>} />
 
+            <Route path='/checkout' element={<CheckOut/>} />
             <Route path='/crear-producto' element={<CrearProducto/>} />
 
           </Routes>
