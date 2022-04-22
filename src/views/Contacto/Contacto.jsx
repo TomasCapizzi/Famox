e094cb3cff23c1cdc849a9dd7dcfd58b9ecb3df7
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-04-22 16:47:45 -0300

    Update Contacto.jsx
    Update contact form to send request by fetch. Validate form with custom hook.

--- src/views/Contacto/Contacto.jsx
+++ src/views/Contacto/Contacto.jsx
@@ -7,19 +7,53 @@ import RedesSociales from 'components/RedesSociales/RedesSociales'
 
 function Contacto() {
 
-  const refNoti = useRef()
+  const refNoti = useRef();
 
-  function mostrarNotificacion(){
-    refNoti.current.classList.toggle('on')
-    setTimeout(() => {
-      refNoti.current.classList.toggle('on')
-  }, 4000);
-  }
+  function enviarConsulta(e){
+    e.preventDefault();
+    // Enviar mail de contacto, de esta manera que lo hacemos no redirecciona a la API
+    const consulta = {
+      nombre:e.target.nombre.value,
+      email: e.target.mail.value,
+      pais: e.target.pais.value,
+      asunto: e.target.asunto.value,
+      empresa: e.target.empresa.value,
+      mensaje: e.target.mensaje.value
+    }
+    console.log(consulta);
+    const request = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(consulta),
+    }
+    fetch('https://famox-api.herokuapp.com/formulario/contacto', request);
+    mostrarNotificacion();
+    resetValores(e);
+}
+
+function resetValores(e){
+  e.target.nombre.value = ''
+  e.target.mail.value = ''
+  e.target.pais.value = ''
+  e.target.asunto.value = ''
+  e.target.empresa.value = ''
+  e.target.mensaje.value = ''
+}
+
+function mostrarNotificacion(){
+  refNoti.current.classList.toggle('on');
+  setTimeout(() => {
+    refNoti.current.classList.toggle('on');
+}, 4000);
+}
+
   return (
     <section className='contacto'>
-        <h1>¡Contactanos!</h1>
+        <h1 id='titulo'>¡Contactanos!</h1>
         <div className='data'>
-            <Form mostrarNotificacion={mostrarNotificacion}/>
+            <Form enviarConsulta={enviarConsulta}/>
             <Info/>
             <Notificacion refNoti={refNoti}/>
         </div>
