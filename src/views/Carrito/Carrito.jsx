873e0006013eb4ad2a9307e87ec78c4ac9389ef1
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-07-12 11:53:42 -0300

    Add new components

--- src/views/Carrito/Carrito.jsx
+++ src/views/Carrito/Carrito.jsx
@@ -1,9 +1,9 @@
 import React, {useContext, useRef, useState} from 'react';
 
 import {CarritoContext} from 'store/carritoContext';
-import CarritoIem from 'components/Carrito/CarritoItem';
-import CarritoItem2 from 'components/Carrito/CarritoItem2';
+import CarritoVacio from 'components/Carrito/CarritoVacio';
 import FormularioCotizacion from 'components/Carrito/FormularioCotizacion';
+import Tabla from 'components/Carrito/Tabla';
 import {useNavigate} from 'react-router-dom';
 
 function Carrito() {
@@ -11,13 +11,12 @@ function Carrito() {
     const {carro, borrarCarrito} = useContext(CarritoContext);
     const [formHandler, setFormHandler] = useState(false)
     const formRef = useRef();
-    const navigate = useNavigate()
+    const navigate = useNavigate();// Funcion nueva del react router para redireccionar
 
     function habilitarForm(){
         setFormHandler(!formHandler);
         formRef.current.classList.toggle('off')
     }
-
     function pedirCotizacion(e){
         // Enviar mail con los datos del pedido. De esta manera evitamos que se redireccione a la API
         e.preventDefault()
@@ -45,37 +44,12 @@ function Carrito() {
         <h1>Carrito</h1>
         <div className='carrito-container'>
             {
-                carro.length === 0 ?
-                    <section className='carro-vacio'>
-                        <h5>Tu carrito esta vacio ...</h5>
-                        <p>Dirígete al <span><a href="/ecommerce">Ecommerce</a></span> para poder seleccionar los productos de tu interés y poder pedir una cotización</p>
-
-                    </section>
+                carro.length === 0 ? <CarritoVacio/>
                 : 
                 <>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Artículo</th>
-                                <th>Detalles</th>
-                                <th>Código</th>
-                                <th>Cantidad</th>
-                                <th>Borrar</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            { 
-                                carro.length >= 1 &&
-                                    carro.map(
-                                        item => item.conector || item.gas || item.modelo ? <CarritoIem item={item} key={item._id} /> : <CarritoItem2 item={item} key={item._id} />
-                                    )
-                            }                    
-                        </tbody>   
-                    </table>
-                    {formHandler ? null : <button onClick={habilitarForm}>Pedir Cotización</button> }
-                    
-                </>
-                
+                    <Tabla carro={carro}/>
+                    {formHandler ? null : <button onClick={habilitarForm}>Pedir Cotización</button> }                    
+                </>                
             }
             {carro.length ? <FormularioCotizacion pedirCotizacion={pedirCotizacion} habilitarForm={habilitarForm}  formRef={formRef} />: null}
         </div>  
