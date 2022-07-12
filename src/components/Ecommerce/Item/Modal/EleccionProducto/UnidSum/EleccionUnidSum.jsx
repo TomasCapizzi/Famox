ac6c79f5b19af9b3353927badb6540190ace5f32
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-07-12 11:24:23 -0300

    Update EleccionUnidSum.jsx
    New atomic components

--- src/components/Ecommerce/Item/Modal/EleccionProducto/UnidSum/EleccionUnidSum.jsx
+++ src/components/Ecommerce/Item/Modal/EleccionProducto/UnidSum/EleccionUnidSum.jsx
@@ -1,13 +1,13 @@
-import React, {useRef, useState} from 'react'
+import React, {useState} from 'react'
 
-import BajaTension from './BajaTension';
+import BajaTension from './Opciones/BajaTension';
 import Botonera from '../Botonera'
-import Conexiones from './Conexiones';
-import GasConector from './GasConector';
-import Iluminacion from './Iluminacion';
-import MediaTension from './MediaTension';
-import Selecciones from './Selecciones';
-import useAgregarCarrito from '../../../../../../hooks/ecommerce/useAgregarCarrito';
+import ContainerSelecciones from './Selecciones/ContainerSelecciones';
+import GasConector from './Opciones/GasConector';
+import Iluminacion from './Opciones/Iluminacion';
+import LongitudPanel from './LongitudPanel';
+import MediaTension from './Opciones/MediaTension';
+import useAgregarCarrito from 'hooks/ecommerce/useAgregarCarrito';
 
 function EleccionUnidSum({item,toggleModal }) {
 
@@ -23,15 +23,10 @@ function EleccionUnidSum({item,toggleModal }) {
       setValoresIluminacion,
       setConexiones,
       setLongitudPanel
-      } = useAgregarCarrito({item})
-      
+      } = useAgregarCarrito({item})   
 
     const [handlerInput, setHandlerInput] = useState(true)
-
-    // Panel de cabecera - Longitud:
-    const medidasRef = useRef();
     
-
     function eliminarItem(item){
       const itemMediaTension = valoresMediaTension.find(
         valor=> valor === item
@@ -63,20 +58,6 @@ function EleccionUnidSum({item,toggleModal }) {
         ))
       }
     }
-
-    function longitudPanelCabecera(e){
-      if(medidasRef.current.value !== 'default'){
-        setLongitudPanel(`${e.target.value} ${medidasRef.current.value}`)
-      }      
-    }
-    function controlarUnidadMedida(){
-      if(medidasRef.current.value === 'default'){
-        setHandlerInput(true)
-      } else {
-        setHandlerInput(false)
-      }
-    }
-
   return (
     <div className='modal-eleccion'>
         <div className='container' >
@@ -86,7 +67,7 @@ function EleccionUnidSum({item,toggleModal }) {
           }
           { // Todos los productos menos el Panel de Cabecera y el Panel de Gases
               item.mediaTension ? 
-              <MediaTension 
+              <MediaTension
                 items={item.mediaTension} 
                 valoresMediaTension={valoresMediaTension} 
                 setValoresMediaTension={setValoresMediaTension}
@@ -107,123 +88,26 @@ function EleccionUnidSum({item,toggleModal }) {
               setValoresIluminacion={setValoresIluminacion}
               /> : null
           }
-
           { // Panel de Cabecera
-            item.nombre === 'Panel de Cabecera' && PanelLongitud(medidasRef,longitudPanelCabecera, controlarUnidadMedida, longitudPanel, handlerInput)
+            item.nombre === 'Panel de Cabecera' && <LongitudPanel setLongitudPanel={setLongitudPanel} longitudPanel={longitudPanel} handlerInput={handlerInput} setHandlerInput={setHandlerInput} />
           }
-
-
-            {///////////////////////////// SELECCIONES ////////////////////////////////////
-            }
-            <div className='selecciones-container'>
-              {
-                conexiones.length >= 1 ?
-                <>
-                  <p>Conexiones</p>
-                  <table className='seleccion-conex'>
-                    <thead>
-                      <tr>
-                        <th>Conector</th>
-                        <th>Gas</th>
-                        <th>Cantidad</th>
-                        <th>Borrar</th>
-                      </tr>
-                    </thead>
-                    <Conexiones conexiones={conexiones} eliminarItem={eliminarItem} />
-                  </table>
-                </>
-
-                : null
-              }
-              {
-                valoresMediaTension.length >= 1 ?
-                <>
-                  <p>Media Tension</p>
-                  <table className='seleccion'>
-                    <thead>
-                      <tr>
-                        <th>Nombre</th>
-                        <th>Cantidad</th>
-                        <th>Borrar</th>
-                      </tr>
-                    </thead>
-                    <Selecciones valores={valoresMediaTension} eliminarItem={eliminarItem} />
-                  </table>
-                </>
-
-                  : null
-              }
-              {
-                valoresBajaTension.length >= 1 ?
-                <>
-                  <p>Baja Tension</p>
-                  <table className='seleccion'>                    
-                    <thead>
-                      <tr>
-                        <th>Nombre</th>
-                        <th>Cantidad</th>
-                        <th>Borrar</th>
-                      </tr>
-                    </thead>
-                    <Selecciones valores={valoresBajaTension} eliminarItem={eliminarItem} />
-                  </table>
-                </>
-                  : null
-              }
-              {
-                valoresIluminacion.length >= 1 ?
-                <>
-                  <p>Iluminación</p>
-                  <table className='seleccion'>
-                    <thead>
-                      <tr>
-                        <th>Nombre</th>
-                        <th>Cantidad</th>
-                        <th>Borrar</th>
-                      </tr>
-                    </thead>
-                    <Selecciones valores={valoresIluminacion} eliminarItem={eliminarItem} />
-                  </table>
-                </>
-                
-                  : null
-              }
-            </div>
+          <ContainerSelecciones conexiones={conexiones} eliminarItem={eliminarItem} valoresBajaTension={valoresBajaTension} valoresIluminacion={valoresIluminacion} valoresMediaTension={valoresMediaTension} />
         </div>
         {
-              // HABILITACION DE BOTONERA
-              (item.nombre === 'Panel de Cabecera') ?
-                ((valoresMediaTension.length > 0 || valoresBajaTension.length > 0 || valoresIluminacion.length > 0) && longitudPanel && (conexiones.length>0) ) ? <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} /> : <p>Seleccionar características y longitud</p>
-              : (item.nombre === 'Panel de Gases') ? (conexiones.length > 0) ? <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal= {toggleModal} /> : <p>Seleccionar conexiones</p>
-              :
-               ((valoresMediaTension.length > 0 || valoresBajaTension.length > 0 || valoresIluminacion.length > 0)&& conexiones.length > 0) ?
-                  <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal}/>
-                : null
-            }
+          // HABILITACION DE BOTONERA
+          (item.nombre === 'Panel de Cabecera') ?
+            ((valoresMediaTension.length > 0 || valoresBajaTension.length > 0 || valoresIluminacion.length > 0) && longitudPanel && (conexiones.length>0) ) ? <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} /> : <p>Seleccionar características y longitud</p>
+          : (item.nombre === 'Panel de Gases') ? (conexiones.length > 0) ? <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal= {toggleModal} /> : <p>Seleccionar conexiones</p>
+          :
+            ((valoresMediaTension.length > 0 || valoresBajaTension.length > 0 || valoresIluminacion.length > 0)&& conexiones.length > 0) ?
+              <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal}/>
+            : null
+        }
     </div>
   )
 }
 
-function PanelLongitud(medidasRef,longitudPanelCabecera, controlarUnidadMedida, longitudPanel, handlerInput){
-  return (
-    <div className='longitud-panel'>
-      <p>Longitud de Panel</p>
-      <div>
-        <input type="number" min={0} onChange={(e)=> longitudPanelCabecera(e)} disabled={handlerInput} />
-        <select name="medidas" id="" ref={medidasRef} onChange={()=> controlarUnidadMedida()} >
-          <option value="default">Elegir unidad de medida</option>
-          <option value="mm">mm</option>
-          <option value="cm">cm</option>
-          <option value="mts">mts</option>
-        </select>
-      </div>
-      {
-        longitudPanel ? <p>{`${longitudPanel}`}</p> : <p>Selecciona una unidad de medida. Longitud mínima 600mm / 60cm</p>
-      }
-    </div>
 
-  )
-}
 
 
 export default EleccionUnidSum;
