6897d3e742fd873ff512c5302662b5d27d81949e
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-07-12 11:30:50 -0300

    Create TablaConexion.jsx
    Table to display chosen conectiones

---
+++ src/components/Ecommerce/Item/Modal/EleccionProducto/UnidSum/Selecciones/TablaConexion.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function TablaConexion() {
+  return (
+    <thead>
+        <tr>
+        <th>Conector</th>
+        <th>Gas</th>
+        <th>Cantidad</th>
+        <th>Borrar</th>
+        </tr>
+    </thead>
+  )
+}
+
+export default TablaConexion
