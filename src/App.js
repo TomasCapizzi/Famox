06d919f8b7a9e55c3bc2e93a4fa23201d06f1aa6
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-07-12 11:12:22 -0300

    Routes container

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import './styles/main.scss';
 
-import {BrowserRouter, HashRouter, Route, Routes} from 'react-router-dom'
-
-import Accesorios from 'views/Productos/Accesorios/Accesorios';
-import Carrito from 'views/Carrito/Carrito';
+import {BrowserRouter} from 'react-router-dom'
 import { CarritoContextProvider } from 'store/carritoContext';
-import Certificaciones from 'views/Certificaciones/Certificaciones';
-import CheckOut from 'views/CheckOut/CheckOut';
-import Contacto from 'views/Contacto/Contacto';
-import CrearProducto from 'views/CrearProducto/CrearProducto';
-import DetalleGasoterapia from 'views/ProductoDetalle/DetalleGasoterapia';
-import DetalleUnidSum from 'views/ProductoDetalle/DetalleUnidSum';
-import Ecommerce from 'views/Ecommerce/Ecommerce';
 import Footer from 'components/Footer/Footer';
-import Gasoterapia from 'views/Productos/Gasoterapia/Gasoterapia';
-import Historia from 'views/Nosotros/Historia';
-import Home from 'views/Home/Home';
-import Login from 'views/Login/Login';
 import Menu from 'components/Menu/Menu';
-import PregFrec from 'views/PregFrec/PregFrec';
-import Productos from 'views/Productos/Productos';
 import React from 'react'
-import UnidadesSuministro from 'views/Productos/UnidadesSuministro/UnidadesSuministro';
+import Rutas from 'components/Rutas/Rutas';
 
 function App() {
   return (
@@ -29,29 +13,7 @@ function App() {
       <BrowserRouter>
         <div className="App">
           <Menu/>
-          <Routes>
-            <Route path='/' element={<Home/>} />
-
-            <Route path='/nosotros' element={<Historia/>} />
-            <Route path='/certificaciones' element={<Certificaciones/>} />
-            <Route path='/contacto' element={<Contacto/>} />
-            <Route path='/productos' element={<Productos/>} />
-            <Route path='/login' element={<Login/>} />
-            <Route path='/ecommerce' element={<Ecommerce/>} />
-            <Route path='/carrito' element={<Carrito/>} />
-            <Route path='/preguntas-frecuentes' element={<PregFrec/>} />
-
-            <Route path='/gasoterapia' element={<Gasoterapia/>}/>
-            <Route path='/unidades-de-suministro' element={<UnidadesSuministro/>}/>
-            <Route path='/accesorios' element={<Accesorios/>}/>
-
-            <Route path='/unidades-de-suministro/:id' element={<DetalleUnidSum/>} />
-            <Route path='/gasoterapia/:id' element={<DetalleGasoterapia/>} />
-
-            <Route path='/checkout' element={<CheckOut/>} />
-            <Route path='/crear-producto' element={<CrearProducto/>} />
-
-          </Routes>
+          <Rutas/>
           <Footer/>
         </div>
       </BrowserRouter>
