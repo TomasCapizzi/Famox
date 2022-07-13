b4ed30263150e4f16900264ff805cccc4b5d1fa1
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-07-13 10:36:00 -0300

    Update UnidSum.jsx

--- src/components/Home/Descripcion/Tareas/UnidSum.jsx
+++ src/components/Home/Descripcion/Tareas/UnidSum.jsx
@@ -6,7 +6,7 @@ function UnidSum() {
         <p className='nro'>02</p>
         <div>
             <h2>Unidades de Suministro</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, a provident placeat nulla blanditiis minus, fuga et delectus cum aspernatur non harum nihil molestias necessitatibus animi quibusdam similique, odit expedita!</p>
+            <p>Proyectamos, diseñamos y fabricamos unidades de suministro cuyo concepto principal es satisfacer las necesidades del profesional de la salud en cualquier tipo de servicio, mediante equipos tales como Paneles de cabecera, Islas de Neonatoloía, Columnas de quirófano y Brazos suspendidos para áreas críticas.</p>
         </div>
     </article>
   )
