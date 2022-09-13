416ace5b88d351ce39286ad05118d68f653b0c5d
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-13 13:51:47 -0300

    New components for contact view

---
+++ src/components/Contacto/Info/DatosContacto/Mails.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Mails() {
+  return (
+    <>
+        <div>
+            <h3>Ventas</h3>
+            <p>ventas@famox.com.ar</p>
+        </div>
+        <div>
+            <h3>Atención al Cliente</h3>
+            <p>atencion.cliente@famox.com.ar</p>
+        </div>
+        <div>
+            <h3>Administración</h3>
+            <p>info@famox.com.ar</p>
+        </div>
+    </>
+  )
+}
+
+export default Mails
---
+++ src/components/Contacto/Info/DatosContacto/Telefono.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Telefono() {
+  return (
+    <div>
+        <h3>Teléfono</h3>
+        <p>4687-8417//6073-6020</p>
+        <p>+54 9 11 3427-6306</p>
+    </div>
+  )
+}
+
+export default Telefono
