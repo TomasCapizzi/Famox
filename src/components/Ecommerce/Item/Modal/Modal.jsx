5c66f71baf241dddc1564a752fffdf7e80de5665
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-13 11:29:43 -0300

    Add accesories feature

--- src/components/Ecommerce/Item/Modal/Modal.jsx
+++ src/components/Ecommerce/Item/Modal/Modal.jsx
@@ -1,18 +1,23 @@
+import React,{useRef} from 'react'
+
 import Descripcion from './Descripcion';
+import EleccionAccesorio from './EleccionProducto/Accesorios/EleccionAccesorio';
 import EleccionGasoterapia from './EleccionProducto/Gasoterapia/EleccionGasoterapia';
 import EleccionUnidSum from './EleccionProducto/UnidSum/EleccionUnidSum';
-import React from 'react'
 import {TiDelete} from 'react-icons/ti';
 
 function Modal({item, toggleModal}) {
+  const containerModalRef = useRef();
+  const modalWinwowRef = useRef()
   return (
-    <section className='modal-container'>
-      <article className='modal'>
+    <section className='modal-container' ref={containerModalRef} onClick={(e)=> toggleModal(e,containerModalRef, modalWinwowRef)}>
+      <article className='modal' ref={modalWinwowRef} onClick={(e)=> toggleModal(e,containerModalRef, modalWinwowRef)}>
         <button onClick={()=> toggleModal()} className='btn-cerrar'><TiDelete/></button>
         <Descripcion item={item} />
         {
-          item.gasoterapia ? <EleccionGasoterapia item={item} toggleModal={toggleModal} /> : <EleccionUnidSum item={item} toggleModal={toggleModal} />
-        }        
+          item.gasoterapia ? <EleccionGasoterapia item={item} toggleModal={toggleModal} /> :  item.accesorio ? <EleccionAccesorio item={item} toggleModal={toggleModal} /> : <EleccionUnidSum item={item} toggleModal={toggleModal} />
+        }
+
       </article>      
     </section>
   )
