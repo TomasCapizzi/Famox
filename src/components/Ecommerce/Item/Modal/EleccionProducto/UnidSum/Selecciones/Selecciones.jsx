d9bacb63013fa634509d411520224cde98155bfc
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-13 11:31:01 -0300

    Indent code

--- src/components/Ecommerce/Item/Modal/EleccionProducto/UnidSum/Selecciones/Selecciones.jsx
+++ src/components/Ecommerce/Item/Modal/EleccionProducto/UnidSum/Selecciones/Selecciones.jsx
@@ -3,16 +3,16 @@ import {TiDelete} from 'react-icons/ti';
 
 function Selecciones({valores, eliminarItem}) {
   return (
-        <tbody className='container'>
+    <tbody className='container'>
       {
-      valores.map(
-        item => item.cantidad > 0 &&
-        <tr key={item.nombre} className='item'>
-          <td>{item.nombre}</td>
-          <td>{item.cantidad}</td>
-          <td><TiDelete onClick={()=>eliminarItem(item)} /></td>
-        </tr>
-    )}
+        valores.map(
+          item => item.cantidad > 0 &&
+            <tr key={item.nombre} className='item'>
+              <td>{item.nombre}</td>
+              <td>{item.cantidad}</td>
+              <td><TiDelete onClick={()=>eliminarItem(item)} /></td>
+            </tr>
+        )}
     </tbody>
 
   )
