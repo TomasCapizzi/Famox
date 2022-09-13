a25700aa08abadf1d938757ebb75c7265a6320da
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-13 11:29:04 -0300

    Add manual button in component

--- src/components/ListaProductos/Accesorios/Item.jsx
+++ src/components/ListaProductos/Accesorios/Item.jsx
@@ -1,13 +1,17 @@
+import ManualBtn from 'components/Detalle/ManualBtn'
 import React from 'react'
 
 function Item({product}) {
   return (
     <article className='accesorio'>
+      {product.manual && <ManualBtn product={product} />}
+      <div>
         <img src={product.img} alt="accesorio" />
-        <div>
-            <h3>{product.nombre}</h3>
-            <p>{product.descripcion}</p>
-        </div>
+      </div>
+      <div className='acc-descrip'>
+          <h3>{product.nombre}</h3>
+          <p>{product.descripcion}</p>
+      </div>
     </article>
   )
 }
