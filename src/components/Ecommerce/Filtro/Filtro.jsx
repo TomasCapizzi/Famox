4907e40bd1ae7fb587699ed2c2622a8ea47c70bb
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-13 14:00:37 -0300

    Add new filter for accesories

--- src/components/Ecommerce/Filtro/Filtro.jsx
+++ src/components/Ecommerce/Filtro/Filtro.jsx
@@ -3,14 +3,14 @@ import {BiReset, BiSearch} from 'react-icons/bi';
 import React from 'react';
 import useFiltro from 'hooks/ecommerce/useFiltro';
 
-function Filtro({setGasoterapiaHandler, setUnidSumHandler, setHandler}) {
+function Filtro({setGasoterapiaHandler, setUnidSumHandler, setAccesoriosHandler, setHandler}) {
 
     const {filtrar, reset} = useFiltro();
 
     return (
         <article className='filtro-container'>
             <div className='filtro-wrap'>
-                <form action="" onSubmit={(e)=> filtrar(e, setGasoterapiaHandler, setUnidSumHandler, setHandler)} >
+                <form action="" onSubmit={(e)=> filtrar(e, setGasoterapiaHandler, setUnidSumHandler, setAccesoriosHandler, setHandler)} >
                     <div className='filtro'>
                         <div>
                             <input type="checkbox" id='gasoterapia' defaultChecked />
@@ -19,14 +19,18 @@ function Filtro({setGasoterapiaHandler, setUnidSumHandler, setHandler}) {
                         <div >
                             <input type="checkbox" id='unidsuministro' defaultChecked />
                             <label htmlFor="unid-suministro">Unidades Suministro</label>
-                        </div>                
+                        </div>         
+                        <div >
+                            <input type="checkbox" id='accesorios' defaultChecked />
+                            <label htmlFor="accesorios">Accesorios</label>
+                        </div>        
                     </div>
                     <button type='submit' className='submit-btn'>
                         Buscar <BiSearch/>
                     </button>
                 
                 </form>
-                <button onClick={()=>reset(setGasoterapiaHandler, setUnidSumHandler, setHandler)} >Resetear búsqueda <BiReset/></button>
+                <button onClick={()=>reset(setGasoterapiaHandler, setUnidSumHandler, setAccesoriosHandler, setHandler)} >Resetear búsqueda <BiReset/></button>
             </div>
 
         </article>
