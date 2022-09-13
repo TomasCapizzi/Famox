1caf128178d2836b4a1854f07b01c65f294c1b0d
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-13 13:51:19 -0300

    Banner component for checkout view

---
+++ src/components/CheckOut/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <img src="img/Banners/Checkout.jpg" alt="checkout" />
+        <h5>Gracias</h5>
+        <p>por elegirnos</p>
+    </div>
+  )
+}
+
+export default Banner
