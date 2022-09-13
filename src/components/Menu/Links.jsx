d84af64de5f531d2e8d3d88f594803951d05fd32
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-13 11:28:31 -0300

    implement new components

--- src/components/Menu/Links.jsx
+++ src/components/Menu/Links.jsx
@@ -2,7 +2,7 @@ import {RiShoppingCart2Line, RiShoppingCartFill} from 'react-icons/ri';
 
 import {FaUser} from 'react-icons/fa';
 import {Link} from 'react-router-dom';
-import {MdKeyboardArrowDown} from 'react-icons/md';
+import MenuCategorias from './MenuCategorias';
 import React from 'react';
 
 function Links({carro}) {
@@ -18,14 +18,7 @@ function Links({carro}) {
         <Link to='/nosotros'>
         <li>Nosotros</li>            
         </Link>
-        <div className='categorias-handler'>
-            <Link  to='/productos'><li>Productos <MdKeyboardArrowDown/></li></Link>
-            <div className='categorias'>
-            <Link to='/gasoterapia'><li>Gasoterapia</li></Link>
-            <Link to='/unidades-de-suministro'><li>Unidades de Suministro</li></Link>
-            <Link to='/accesorios'><li>Accesorios</li></Link>
-            </div>
-        </div>
+        <MenuCategorias/>
         <Link  to='/certificaciones'>
         <li>Certificaciones</li>            
         </Link>
