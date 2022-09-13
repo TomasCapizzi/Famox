812abdf8954730622c0e3df14d30afdc27b4984c
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-13 11:29:23 -0300

    Delete code and replace it with hook

--- src/components/Home/Carrusel2/CarruselMini.jsx
+++ src/components/Home/Carrusel2/CarruselMini.jsx
@@ -2,6 +2,7 @@ import {MdOutlineArrowBackIosNew, MdOutlineArrowForwardIos} from 'react-icons/md
 import React, {useRef} from 'react';
 
 import Imagen from './Imagen';
+import useCarrusel2 from 'hooks/carrusel/useCarrusel2';
 
 function CarruselMini() {
 
@@ -9,24 +10,9 @@ function CarruselMini() {
     const btnSiguiente = useRef();
     const containerRef = useRef();
     const contenidoRef = useRef();
-
-    function mover(valor){
-        // Track
-        const anchoContenido = contenidoRef.current.offsetWidth;
-        // List
-        const anchoContainer = containerRef.current.offsetWidth;  
-
-        let leftPosition;
-
-        contenidoRef.current.style.left === '' ? leftPosition = contenidoRef.current.style.left = 0 : leftPosition = parseFloat(contenidoRef.current.style.left.slice(0,-2)*-1)
-
-        if(leftPosition < (anchoContenido - anchoContainer) && valor === 2){
-            contenidoRef.current.style.left = `${-1 * (leftPosition + contenidoRef.current.childNodes[1].offsetWidth )}px`
-        } else if(leftPosition > 0 && valor === 1){
-            contenidoRef.current.style.left = `${-1 * (leftPosition - contenidoRef.current.childNodes[1].offsetWidth )}px`
-        }
-    }
-
+    
+    const {mover} = useCarrusel2(contenidoRef, containerRef);
+    
   return (
     <section className='carrusel-mini'>
         <h2>Conocenos</h2>
