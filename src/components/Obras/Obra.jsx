616a45eb7cd854e4f4a3a8be574c3a9dab93fba1
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-10-31 11:35:46 -0300

    Obra component

---
+++ src/components/Obras/Obra.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Obra({obra}) {
+  return (
+    <article className='obra'>
+      <div>
+        <h3>{obra.obra}</h3>
+        <p>Fecha: {obra.fecha}</p>
+      </div>
+      <img src={obra.img} alt="" />
+      <h5>Instalación</h5>
+      <ul>
+        {
+            obra.instalacion.map(
+                inst => <li key={inst}>{inst}</li>
+            )
+        }
+      </ul>
+
+    </article>
+  )
+}
+
+export default Obra
