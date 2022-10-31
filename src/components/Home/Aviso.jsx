5dc7f067247a17b5e3244964b3fc5d4b2c911b40
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-10-31 11:34:28 -0300

    Change advertising of Home

--- src/components/Home/Aviso.jsx
+++ src/components/Home/Aviso.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import {HiOutlineDownload} from 'react-icons/hi'
+import React from 'react';
 
 function Aviso() {
   return (
     <article className='aviso'>
-        <button><a href="/ecommerce">Ir al Ecommerce</a></button>
+      <img src="img/Banners/Catalogo.jpg" alt="Catalogo" />
+      <div>
+        <p>¡Nuevo Catálogo!</p>
+        <button><a href="http://famox-env.eba-8tvz54ez.sa-east-1.elasticbeanstalk.com/catalogo/Catalogo.pdf" target='_black'>Descargar <HiOutlineDownload/></a></button>
+      </div>
+        
     </article>
   )
 }
