cc647d9a025a6aeadeb17a73fcfeab89067d6ae6
Tomas Capizzi <tomas.capizzi@hotmail.com>
2023-03-27 15:07:01 -0300

    Update Tabla.jsx

--- src/components/Carrito/Tabla.jsx
+++ src/components/Carrito/Tabla.jsx
@@ -6,23 +6,23 @@ import React from 'react';
 function Tabla({carro}) {
   return (
     <table>
-    <thead>
-        <tr>
-            <th>Artículo</th>
-            <th>Detalles</th>
-            <th>Código</th>
-            <th>Cantidad</th>
-            <th>Borrar</th>
-        </tr>
-    </thead>
-    <tbody>
-        { 
-            carro.length >= 1 &&
-                carro.map(
-                    item => item.conector || item.gas || item.modelo ? <CarritoItemGas item={item} key={item._id} /> : item.accesorio ? <CarritoItemAcc item={item} key={item._id} /> : <CarritoItemUS item={item} key={item._id} />
-                )
-        }                    
-    </tbody>   
+        <thead>
+            <tr>
+                <th>Artículo</th>
+                <th>Detalles</th>
+                <th>Código</th>
+                <th>Cantidad</th>
+                <th>Borrar</th>
+            </tr>
+        </thead>
+        <tbody>
+            { 
+                carro.length >= 1 &&
+                    carro.map(
+                        item => item.conector || item.gas || item.modelo ? <CarritoItemGas item={item} key={item._id} /> : item.accesorio ? <CarritoItemAcc item={item} key={item._id} /> : <CarritoItemUS item={item} key={item._id} />
+                    )
+            }                    
+        </tbody>   
 </table>
   )
 }
