35c534140927618a7eea7c490508b2c0ba6bc8d3
Tomas Capizzi <tomas.capizzi@hotmail.com>
2023-06-01 14:02:49 -0300

    Order product list by id

--- src/hooks/productos/useListaProductos.js
+++ src/hooks/productos/useListaProductos.js
@@ -6,7 +6,7 @@ const useListaProductos = () => {
     async function getProducts(api){
         const response = await fetch(api);
         const res = await response.json();
-        setProducts(res.products);
+        setProducts(res.products.sort((a,b) => a.orden - b.orden));
     }
   return {
     getProducts,
