0aec51affdb0503aa4ccc70214e848039b8d8643
Tomas Capizzi <tomas.capizzi@hotmail.com>
2023-09-25 14:38:05 -0300

    Update FotoSlider.jsx

--- src/components/Obras/FotoSlider.jsx
+++ src/components/Obras/FotoSlider.jsx
@@ -1,5 +1,5 @@
 import {BsArrowLeftCircle, BsArrowRightCircle} from 'react-icons/bs'
-import React, {useRef, useState} from 'react'
+import React, {useState} from 'react'
 
 function FotoSlider({obra}) {
 
