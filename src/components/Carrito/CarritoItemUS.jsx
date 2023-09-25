d3ce97d7c6ed56c3b16fb6a94efc61b35a6e82e2
Tomas Capizzi <tomas.capizzi@hotmail.com>
2023-09-25 13:31:12 -0300

    Implement New Hook

--- src/components/Carrito/CarritoItemUS.jsx
+++ src/components/Carrito/CarritoItemUS.jsx
@@ -4,18 +4,14 @@ import React, {useContext, useRef, useState} from 'react';
 import {BsTrashFill} from 'react-icons/bs';
 import {CarritoContext} from 'store/carritoContext';
 import DetallesUnidSum from './DetallesUnidSum';
+import useMostrarCaracteristicas from 'hooks/cart/useMostrarCaracteristicas';
 
 function CarritoItem2({item}) {
 // Carrito Item 2 para los equipos de UNIDADES DE SUMINISTRO
   const {removerItem} = useContext(CarritoContext);
   const [handlerInfo, setHandlerInfo] = useState(false);
-
-  const caractRef = useRef()
-
-  const mostrarCaracteristicas = ()=> {
-      setHandlerInfo(!handlerInfo);
-      caractRef.current.classList.toggle('on');
-  }
+  const caractRef = useRef();
+  const {mostrarCaracteristicas} = useMostrarCaracteristicas();
 
   return (
     <>
@@ -23,13 +19,12 @@ function CarritoItem2({item}) {
             <td><img src={item.img} alt="" /><p>{item.nombre}</p></td>
             <td>
                 {
-                    handlerInfo ? <AiFillCloseCircle className='info' onClick={mostrarCaracteristicas}/> : <AiFillInfoCircle className='info' onClick={mostrarCaracteristicas}/> 
+                    handlerInfo ? <AiFillCloseCircle className='info' onClick={() => mostrarCaracteristicas(setHandlerInfo, handlerInfo, caractRef)}/> : <AiFillInfoCircle className='info' onClick={() => mostrarCaracteristicas(setHandlerInfo, handlerInfo, caractRef)}/> 
                 }
             </td>
             <td><p> - </p></td>
             <td><p>{item.cantidad}</p></td>
             <td><BsTrashFill onClick={()=> removerItem(item)} /></td>
-
         </tr>
         <DetallesUnidSum item={item} caractRef={caractRef} />
     </>
