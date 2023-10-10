d80c7c1cbd401595ca06185ee8671ea4741dba46
Tomas Capizzi <tomas.capizzi@hotmail.com>
2023-10-10 15:07:22 -0300

    Add new components and refactor code

--- src/components/Ecommerce/Item/Modal/EleccionProducto/Gasoterapia/EleccionGasoterapia.jsx
+++ src/components/Ecommerce/Item/Modal/EleccionProducto/Gasoterapia/EleccionGasoterapia.jsx
@@ -1,96 +1,45 @@
-import Botonera from '../Botonera'
-import ConectorSeleccion from './Selecciones/ConectorSeleccion'
 import Conectores from './Opciones/Conector/Conectores'
-import GasSeleccion from './Selecciones/GasSeleccion'
 import Gases from './Opciones/Gas/Gases'
-import ModeloSeleccion from './Selecciones/ModeloSeleccion'
 import Modelos from './Opciones/Modelo/Modelos'
 import Rango from './Opciones/Rango/Rango'
-import RangoSeleccion from './Selecciones/RangoSeleccion'
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 import useAgregarCarrito from 'hooks/ecommerce/useAgregarCarrito'
 import useEliminarSelecciones from 'hooks/ecommerce/gasoterapia/useEliminarSelecciones'
+import SeleccionContainer from './Selecciones/SeleccionContainer'
+import NotificacionAgregadoModelo from './Opciones/Modelo/NotificacionAgregadoModelo'
+import HabilitacionBotonera from './HabilitacionBotonera'
 
 function EleccionGasoterapia({item,toggleModal }) {
 
+  const refNoti = useRef();
   const {agregarAlCarrito, setConector, setGas, setModelo, setRango, modelo, conector, gas, rango} = useAgregarCarrito({item});
 
   const {eliminarSeleccionConector, eliminarSeleccionGas, eliminarSeleccionRango, eliminarSeleccionModelo} = useEliminarSelecciones(setConector, setGas, setRango, setModelo)
 
+  useEffect(()=>{
+    if(item.gases_.length === 1){
+      setGas(item.gases_[0]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   return (
     <div className='modal-eleccion'>
-        <div className='container' >
-          {
-            item.conectores ? <Conectores items={item.conectores} setConector={setConector} /> : null
-          }
-          {
-            item.gases_ ?             
-              <Gases items={item.gases_} setGas={setGas} /> : null 
-          }
-          {
-            item.rangos ?
-              <Rango item={item} setRango={setRango} />: null            
-          }
-          {
-            item.modelos ?
-              <Modelos item={item} setModelo={setModelo} /> : null 
-          }
-          { // Selecciones de Gas, Conectores, Modelos y Rangos
-            item.gases_ || item.conectores || item.modelos || item.rangos ? 
-              <div className='selecciones'>
-                {
-                  gas || conector || rango || modelo ? <p>Tu selección</p> : null
-                }
-                <div className='container'>
-                  {
-                    conector && <ConectorSeleccion conector={conector} eliminarSeleccionConector={eliminarSeleccionConector} />
-                  }
-                  {
-                    gas && <GasSeleccion gas={gas} eliminarSeleccionGas={eliminarSeleccionGas} />
-                  }
-                  {
-                    rango && <RangoSeleccion rango={rango} eliminarSeleccionRango={eliminarSeleccionRango} />
-                  }
-                  {
-                    item.modelos ?
-                        modelo && <ModeloSeleccion nombre={modelo.nombre} eliminarSeleccionModelo={eliminarSeleccionModelo} /> : null
-                  }
-                </div>
-              </div> 
-            : null
-            }            
-        </div>
-        { // Habilitacion de Botonera
-          (item.gases_ && item.conectores && !item.modelos && item.rangos) ?
-            (conector && gas && rango) ?
-              <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
-            : <p>Primero debes seleccionar el tipo de gas y su conexión y rango</p>
-
-          : (!item.rangos && item.gas && item.conexion && !item.modelos ) ?
-            (conector && gas)? <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
-            : <p>Primero debes seleccionar el tipo de gas y su conexión</p>
-
-          : (item.gas && item.conexion === false) ? 
-          (gas) ?
-            <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
-          : <p>Selecciona el tipo de gas</p>
-
-          : (item.gas === false && item.conexion) ?
-          (conector) ?
-            <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
-          : <p>Selecciona el tipo de conector</p>
-
-          : (item.modelos && !item.gas && !item.conexion ) ?
-          modelo ? <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
-          : <p>Elige el modelo</p>
-
-          : (item.gas && item.conexion && item.modelos) ?
-          (conector && gas && modelo) ? 
-          <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
-          : <p>Primero debes seleccionar el tipo de gas,su conexión y el modelo</p>
-
-          : <Botonera agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal} />
-          }
+        {
+          item && 
+          <div className='container' >
+            { item.conectores ? <Conectores items={item.conectores} setConector={setConector} /> : null }
+            { item.gases_.length > 1 ? <Gases items={item.gases_} setGas={setGas} /> : null }
+            { item.rangos ? <Rango item={item} setRango={setRango} />: null }
+            { item.modelos ? <Modelos item={item} setModelo={setModelo} refNoti={refNoti} /> : null }
+            { // Selecciones de Gas, Conectores, Modelos y Rangos
+              item.gases_ || item.conectores || item.modelos || item.rangos ? 
+                <SeleccionContainer gas={gas} conector={conector} rango={rango} modelo={modelo} item={item} eliminarSeleccionConector={eliminarSeleccionConector} eliminarSeleccionGas={eliminarSeleccionGas} eliminarSeleccionRango={eliminarSeleccionRango} eliminarSeleccionModelo={eliminarSeleccionModelo} />
+              : null
+              }
+          </div> 
+        }
+        <HabilitacionBotonera item={item} conector={conector} gas={gas} rango={rango} modelo={modelo} agregarAlCarrito={agregarAlCarrito} toggleModal={toggleModal}/>
+        <NotificacionAgregadoModelo refNoti={refNoti} />
     </div>
   )
 }
