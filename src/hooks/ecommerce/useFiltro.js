48f45150e6923cd32d0d2c529bc947157ed01f71
Tomas Capizzi <tomas.capizzi@hotmail.com>
2023-11-29 09:45:20 -0300

    Update Filter hook
    improve feature

--- src/hooks/ecommerce/useFiltro.js
+++ src/hooks/ecommerce/useFiltro.js
@@ -1,89 +1,55 @@
 
 const useFiltro = () => {
-    function filtrar(e, setGasoterapiaHandler, setUnidSumHandler, setAccesoriosHandler, setHandler){
+    function filtrar(e, setGasoterapiaHandler, setUnidSumHandler, setAccesoriosHandler, setEquiposDigitalesHandler, setHandler, filtroGasoterapiaRef, filtroUnidSumRef, filtroAccesoriosRef, filtroEquiposDigRef){
         e.preventDefault();        
         // Tipos de producto
-        const gasoterapia = e.target.gasoterapia.checked;
-        const unidadesSuministro = e.target.unidsuministro.checked;
-        const accesorios = e.target.accesorios.checked
-        // Busqueda de Gasoterapia
-        if(gasoterapia === true && unidadesSuministro === false && accesorios === false ){
+        const gasoterapia = filtroGasoterapiaRef.current.checked;
+        const unidadesSuministro = filtroUnidSumRef.current.checked;
+        const accesorios = filtroAccesoriosRef.current.checked;
+        const equiposDig = filtroEquiposDigRef.current.checked;
+
+        setAccesoriosHandler(false);
+        setEquiposDigitalesHandler(false);
+        setGasoterapiaHandler(false);
+        setUnidSumHandler(false);
+
+        if(gasoterapia){
             setHandler(false)
             setTimeout(()=>{
                 setGasoterapiaHandler(true)
-                setUnidSumHandler(false)
-                setAccesoriosHandler(false)
                 setHandler(true)
             },800)
         }
-        // Búsqueda de Unidades de Suministro
-        else if(gasoterapia === false && unidadesSuministro === true && accesorios === false){
+        if(unidadesSuministro){
             setHandler(false)
             setTimeout(()=>{
-                setGasoterapiaHandler(false)
                 setUnidSumHandler(true)
-                setAccesoriosHandler(false)
                 setHandler(true)
             },800)
         }
-        // Búsqueda de Accesorios
-        else if(gasoterapia === false && unidadesSuministro === false && accesorios === true){
+        if(accesorios){
             setHandler(false)
             setTimeout(()=>{
-                setGasoterapiaHandler(false)
-                setUnidSumHandler(false)
                 setAccesoriosHandler(true)
                 setHandler(true)
             },800)
         }
-        // Búsqueda de dos categorías
-        else if(gasoterapia === true && unidadesSuministro === true && accesorios === false){
+        if(equiposDig){
             setHandler(false)
             setTimeout(()=>{
-                setGasoterapiaHandler(true)
-                setUnidSumHandler(true)
-                setAccesoriosHandler(false)
+                setEquiposDigitalesHandler(true)
                 setHandler(true)
             },800)
-        }
-        else if(gasoterapia === true && unidadesSuministro === false && accesorios === true){
-            setHandler(false)
-            setTimeout(()=>{
-                setGasoterapiaHandler(true)
-                setUnidSumHandler(false)
-                setAccesoriosHandler(true)
-                setHandler(true)
-            },800)
-        }
-        else if(gasoterapia === false && unidadesSuministro === true && accesorios === true){
-            setHandler(false)
-            setTimeout(()=>{
-                setGasoterapiaHandler(false)
-                setUnidSumHandler(true)
-                setAccesoriosHandler(true)
-                setHandler(true)
-            },800)
-        }
-        else if(gasoterapia === true && unidadesSuministro === true && accesorios === true){
-            setHandler(false)
-            setTimeout(()=>{
-                setGasoterapiaHandler(true)
-                setUnidSumHandler(true)
-                setAccesoriosHandler(true)
-                setHandler(true)
-            },800)
-        }
-        // Busqueda nula
-        else{
-            console.log('No hay categorias seleccionadas');
         }
     }
-    function reset(setGasoterapiaHandler, setUnidSumHandler, setAccesoriosHandler, setHandler){
+    
+    function reset(setGasoterapiaHandler, setUnidSumHandler, setAccesoriosHandler, setEquiposDigitalesHandler, setHandler){
         setHandler(false)
         setTimeout(()=>{
             setGasoterapiaHandler(true)
             setUnidSumHandler(true)
             setAccesoriosHandler(true)
+            setEquiposDigitalesHandler(true);
             setHandler(true)
         },800)
     }
