4ce999578c8516947b53e3f0d3e2cc871060df5b
Tomas Capizzi <tomas.capizzi@hotmail.com>
2024-02-21 13:29:20 -0300

    Add Intersection obs to category

--- src/components/Home/Categoria/Categoria.jsx
+++ src/components/Home/Categoria/Categoria.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Categoria({titulo, img, link}) {
+function Categoria({titulo, img, link, isIntersecting}) {
   return (
-    <div className='categoria'>
+    <div className={isIntersecting ? 'categoria on':'categoria off'}>
         <h3>{titulo}</h3>
         <a href={link}>
             <img src={img} alt="Categoria de producto" />
--- src/components/Home/Categorias.jsx
+++ src/components/Home/Categorias.jsx
@@ -1,13 +1,16 @@
 import Categoria from './Categoria/Categoria'
 import React from 'react'
+import useIntersection from 'hooks/intersectionObserver/useIntersection'
 
 function Categorias() {
+
+  const {isIntersecting, elementRef} = useIntersection();
   return (
-    <div className='categorias-container'>
-        <Categoria titulo='Unidades de Suministro' img='img/Categorias/unidadesSuministro.webp' link='/unidades-de-suministro' />
-        <Categoria titulo='Gasoterapia' img='img/Categorias/gasoterapia.webp' link='/gasoterapia' />
-        <Categoria titulo='Accesorios' img='img/Categorias/accesorios.webp' link='accesorios' />
-        <Categoria titulo='Equipos Digitales' img='img/Categorias/llamadores.webp' link='/equipos-digitales'/>
+    <div className='categorias-container' ref={elementRef}>
+        <Categoria titulo='Unidades de Suministro' img='img/Categorias/unidadesSuministro.webp' link='/unidades-de-suministro' isIntersecting={isIntersecting}/>
+        <Categoria titulo='Gasoterapia' img='img/Categorias/gasoterapia.webp' link='/gasoterapia' isIntersecting={isIntersecting}/>
+        <Categoria titulo='Accesorios' img='img/Categorias/accesorios.webp' link='accesorios' isIntersecting={isIntersecting}/>
+        <Categoria titulo='Equipos Digitales' img='img/Categorias/llamadores.webp' link='/equipos-digitales' isIntersecting={isIntersecting}/>
     </div>
   )
 }
