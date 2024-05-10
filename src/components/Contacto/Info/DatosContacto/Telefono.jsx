4aa3bf067059071da2629af028e42f194a201c92
Tomas Capizzi <tomas.capizzi@hotmail.com>
2024-05-10 13:40:29 -0300

    Update phone number

--- src/components/Contacto/Info/DatosContacto/Telefono.jsx
+++ src/components/Contacto/Info/DatosContacto/Telefono.jsx
@@ -4,7 +4,7 @@ function Telefono() {
   return (
     <div>
         <h3>Teléfono</h3>
-        <p>4687-8417//6073-6020</p>
+        <p>6073-6020</p>
         <p>+54 9 11 3427-6306</p>
     </div>
   )
